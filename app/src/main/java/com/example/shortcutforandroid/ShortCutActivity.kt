f04f1f4bf6c871b7cc12d09c3e824d039e57426c
kalyani <kalyani@scogo.in>
2022-04-08 13:40:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shortcutforandroid/ShortCutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shortcutforandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShortCutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_short_cut)
+    }
+}
